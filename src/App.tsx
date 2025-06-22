13ac5d27bc81d56cdb4e0fbc3a1fbf96c38e2bcd
Yashe777 <mr.oudesign@gmail.com>
2025-06-22 15:18:46 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -28,14 +28,3 @@ const App = () => (
 );
 
 export default App;
-import Appointments from '../components/Appointments';
-
-export default function Dashboard() {
-  return (
-    <main className="p-6">
-      <h1 className="text-2xl font-bold mb-4">Appointments</h1>
-      <Appointments />
-    </main>
-  );
-}
-
